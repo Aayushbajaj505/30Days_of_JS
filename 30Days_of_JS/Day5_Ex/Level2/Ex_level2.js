4e7cc06892d03cd13933a048cfba57b728b21ffb
Aayush Bajaj <aayushbajaj505@gmail.com>
2022-05-29 15:05:34 +0530

    dsc.kl

---
+++ 30Days_of_JS/Day5_Ex/Level2/Ex_level2.js
@@ -0,0 +1,5 @@
+/*jshint esversion: 6 */
+let text =
+'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+console.log(words);
+console.log(words.length);
---
+++ 30Days_of_JS/Day5_Ex/Level2/countries.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ];
