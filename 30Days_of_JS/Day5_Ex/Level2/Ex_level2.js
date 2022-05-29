2f2111cd9e6684bf57e5e39f6632923dab590c1c
Aayush Bajaj <aayushbajaj505@gmail.com>
2022-05-29 16:07:46 +0530

    sdl

--- 30Days_of_JS/Day5_Ex/Level2/Ex_level2.js
+++ 30Days_of_JS/Day5_Ex/Level2/Ex_level2.js
@@ -25,3 +25,10 @@ if (webTechs.indexOf("Sass")) {
  } else webTechs.push("Sass");
 
  console.log(webTechs);
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+const backEnd = ['Node','Express', 'MongoDB'];
+
+let fullStack= frontEnd.concat(backEnd);
+
+console.log(fullStack);
