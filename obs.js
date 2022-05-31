6d83b9420c8773a523f49ac7428e4752eff52ec5
Aayush Bajaj <aayushbajaj505@gmail.com>
2022-05-31 15:45:08 +0530

    dc

--- obs.js
+++ obs.js
@@ -1,13 +1,1 @@
 /*jshint esversion: 6 */
-for(let i=0;i<10;i++) {
-    console.log(i);
- }
-
- for(let i=0;i<10;i++) {
-     console.log("#".repeat(i));
- }
- let arr=[];
- for(let i=0;i<15;i++){
-    console.log(arr.push(parseInt(Math.random()*10)));
- }
- console.log(arr);
