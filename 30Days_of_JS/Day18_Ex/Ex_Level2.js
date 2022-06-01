6e5264dfa73bb7bc122237e05c1e81e380eaac39
Aayush Bajaj <aayushbajaj505@gmail.com>
2022-06-01 15:52:27 +0530

    sfv

---
+++ 30Days_of_JS/Day18_Ex/Ex_Level2.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 8 */
+const catsAPI = 'https://api.thecatapi.com/v1/breeds';
+const fetchCat = async () => {
+    try{
+    const res = await fetch(catsAPI);
+    const cat = await res.json();
+    console.log(cat);
+    }
+    catch(err){
+      console.error(err);
+    }
+  };
+  fetchCat();
